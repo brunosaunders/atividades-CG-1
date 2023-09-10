3d2774c24f253a6e803b3bc2cb9c84e17349006a
Bruno Sena Saunders <senabruno97@hotmail.com>
2023-09-10 19:16:48 -0300

    matriz de cores para evitar cálculos desnecessários

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <sstream>
 
 using namespace std;
 
@@ -33,9 +34,9 @@ void print_vector(std::vector<float> v) {
 
 class Color {
     public:
-        int r; int g; int b;
+        uint8_t r; uint8_t g; uint8_t b;
         Color() {}
-        Color(int red, int green, int blue) {
+        Color(uint8_t red, uint8_t green, uint8_t blue) {
             this->r = red;
             this->g = green;
             this->b = blue;
@@ -90,16 +91,24 @@ class Vector3d {
         }
 };
 
+std::ostream &operator<<(std::ostream &os, Vector3d const &v) {
+    return os << "Vector(" << v.x << "," << v.y << "," << v.z << ")";
+}
+
+std::ostream &operator<<(std::ostream &os, Vector3dSDL const &v) {
+    return os << "VectorSDL(" << v.x << "," << v.y << "," << v.z << ")";
+}
+
 Vector3d Vector3dSDL::to_creto_coord_system(int w_width, int w_height) {
     float new_x = this->x - w_width/2;
-    float new_y = this->y - w_height/2;
+    float new_y = -(this->y) + w_height/2;
 
     return Vector3d(new_x, new_y, this->z);
 }
 
 Vector3dSDL Vector3d::to_sdl_coord_system(int w_width, int w_height) {
     float new_x = this->x + w_width/2;
-    float new_y = this->y + w_height/2;
+    float new_y = -(this->y) + w_height/2;
 
     return Vector3dSDL(new_x, new_y, this->z);
 }
@@ -148,6 +157,10 @@ class Ray {
         }
 };
 
+std::ostream &operator<<(std::ostream &os, Ray const &r) {
+    return os << "Ray(P1: " << r.p1 << " | P2: " << r.p2 << ")";
+}
+
 class Sphere {
     public:
         Vector3d center;
@@ -173,7 +186,7 @@ class Sphere {
             // w² + 2w*tint*dr + tint²*dr² - R² = 0
             // dr²*tint² + 2w*dr*tint + w² -     R² = 0
             float a = dr.scalar_product(dr);
-            float b = w.multiply(2).scalar_product(dr);
+            float b = (w.multiply(2)).scalar_product(dr);
             float c = w.scalar_product(w) - std::pow(this->radius, 2);
 
             float delta = std::pow(b, 2) - (4*a*c);
@@ -194,6 +207,9 @@ class Window {
         // dx and dy of rectangles
         float dx; float dy;
 
+        std::vector<std::vector<Color>> windows_colors;
+        bool should_update = true;
+
         Window(){}
         Window(int width, int height, int cols, int rows, float x, float y, float z) {
             this->width = width; this->height = height;
@@ -202,20 +218,25 @@ class Window {
             
             this->dx = width/(float) cols;
             this->dy = height/(float) rows;
+
+            for (int i=0; i < height; i++) {
+                std::vector<Color> row;
+                for (int j=0; j < height; j++) {
+                    row.push_back(Color(100,100,100));
+                }
+                windows_colors.push_back(row);
+            } 
         }
 };
 
 
 int render_picture(int n_rows, int n_cols, int window_width, int window_height) {
 
-    Window cretos_window(window_width, window_height, n_cols, n_rows, 0, 0, -30);
     Vector3d origin(0, 0, 0);
-
-    // Sphere sphere(0, 0, -1000, 960);
     std::vector<Sphere> objects;
-    // objects.push_back(sphere);
-    objects.push_back(Sphere(810, 0, -950, 900));
-    objects.push_back(Sphere(-810, 0, -950, 800));
+    Window cretos_window(window_width, window_height, n_cols, n_rows, 0, 0, -10);
+    
+    objects.push_back(Sphere(0, 0, -3000, 2998));
 
 
     // Initialize library
@@ -250,6 +271,10 @@ int render_picture(int n_rows, int n_cols, int window_width, int window_height) 
 
     bool isRunning = true;
     SDL_Event event;
+
+    // using pointers to save memory inside the loop
+    Ray* ray = new Ray();
+    Vector3d* center_of_small_rectangle = new Vector3d();
     
     while (isRunning) {
         // Event listening
@@ -257,13 +282,43 @@ int render_picture(int n_rows, int n_cols, int window_width, int window_height) 
             if (event.type == SDL_QUIT) {
                 isRunning = false;
             }
+
+            if (event.type == SDL_KEYUP) {
+                cretos_window.center.z--; 
+                cout << cretos_window.center << endl;
+                cretos_window.should_update = true;
+            }
         }
 
         // Good practice
         SDL_RenderClear(renderer);
 
-        int counter = 0;
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+        // Just draw the colors saved in cretos_window.windows_colors
+        if (!cretos_window.should_update) {
+            for (int l = 0; l < n_rows; l++) {
+                float y = cretos_window.height/2 - (cretos_window.dy/2) - (cretos_window.dy*l); // Creto's system
+                
+                for (int c = 0; c < n_cols; c++) {
+                    float x = - cretos_window.width/2 + (cretos_window.dx/2) + (cretos_window.dx*c); // Creto's system
+
+                    center_of_small_rectangle->x = x;
+                    center_of_small_rectangle->y = y;
+                    center_of_small_rectangle->z = cretos_window.center.z;
+                    Vector3dSDL vecSDL = center_of_small_rectangle->to_sdl_coord_system(cretos_window.width, cretos_window.height);
+
+                    Color color = cretos_window.windows_colors[vecSDL.y][vecSDL.x];
+               
+
+                    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
+                    SDL_RenderDrawPoint(renderer, vecSDL.x, vecSDL.y);
+                }
+            }
+            // Lastly, we update the window with the renderer we just painted
+            SDL_RenderPresent(renderer);
+            continue;
+        }
 
         // By default, we will always use Creto's system to calculate, when we need to draw just transform to SDL system
         for (int l = 0; l < n_rows; l++) {
@@ -271,33 +326,57 @@ int render_picture(int n_rows, int n_cols, int window_width, int window_height) 
 
             for (int c = 0; c < n_cols; c++) {
                 float x = - cretos_window.width/2 + (cretos_window.dx/2) + (cretos_window.dx*c); // Creto's system
-                Vector3d center_of_small_rectangle(x,y, cretos_window.center.z);
-                Ray ray(origin, center_of_small_rectangle);
-                Color color = Color(100, 100, 100);
 
-                if (objects[0].intersected_by(ray) || objects[1].intersected_by(ray)) {
-                    color = objects[0].color;
+                center_of_small_rectangle->x = x;
+                center_of_small_rectangle->y = y;
+                center_of_small_rectangle->z = cretos_window.center.z;
+
+                ray->p1 = origin;
+                ray->p2 = *center_of_small_rectangle;
+
+                Vector3dSDL vecSDL = center_of_small_rectangle->to_sdl_coord_system(cretos_window.width, cretos_window.height);
+                cretos_window.windows_colors[vecSDL.y][vecSDL.x] = Color(100,100,100);
+
+                for (auto& object : objects) {
+                    if (object.intersected_by(*ray)) {
+                        cretos_window.windows_colors[vecSDL.y][vecSDL.x] = object.color;
+                    }
                 }
 
-
+                Color color = cretos_window.windows_colors[vecSDL.y][vecSDL.x];
+               
                 SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
-
-                Vector3dSDL vecSDL = center_of_small_rectangle.to_sdl_coord_system(cretos_window.width, cretos_window.height);
-                SDL_RenderDrawPointF(renderer, vecSDL.x, vecSDL.y);
+                SDL_RenderDrawPoint(renderer, vecSDL.x, vecSDL.y);
             }
         }
 
         // Lastly, we update the window with the renderer we just painted
         SDL_RenderPresent(renderer);
+        cretos_window.should_update = false;
     }
 
     // Free the memory
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+    delete ray;
+    delete center_of_small_rectangle;
     return 0;
 }
 
+
+void tests() {
+    Vector3d v(16,28,10);
+    Vector3d v2(22,36,10);
+
+    Ray r(v,v2);
+    cout << r << endl;
+    cout << r.get_dr() << endl;
+
+    // std::cout << v < std::endl;
+}
+
 int main(int argc, char* argv[]) {
-    return render_picture(1200, 600, 1200, 600);
+    // tests();
+    return render_picture(700, 700, 700, 700);
 }
