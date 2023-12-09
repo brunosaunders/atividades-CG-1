c0da88d571884a0a6173cf01220823f14204ed69
Guilherme Gomes Botelho <95437442+guimagedon@users.noreply.github.com>
2023-12-09 10:35:23 -0300

    Update Objects.hpp

--- include/Objects.hpp
+++ include/Objects.hpp
@@ -18,7 +18,7 @@ namespace atividades_cg_1::objects {
         virtual ~Object() {}
 
         Object(Color color, IntensityColor dr, IntensityColor sr, IntensityColor er, float shininess)
-        : color(color), difuse_reflectivity(dr), specular_reflectivity(sr), environment_reflectivity(er), shininess(shininess) {}
+                : color(color), difuse_reflectivity(dr), specular_reflectivity(sr), environment_reflectivity(er), shininess(shininess) {}
 
         Color color;
         IntensityColor difuse_reflectivity;   // K_d
@@ -42,7 +42,7 @@ namespace atividades_cg_1::objects {
         float radius;
 
         Sphere(Vector3d center, float radius, Color color, IntensityColor difuse_reflectivity, IntensityColor specular_reflectivity, IntensityColor environment_reflectivity, float shininess)
-        : Object(color, difuse_reflectivity, specular_reflectivity, environment_reflectivity, shininess), center(center), radius(radius) {}
+                : Object(color, difuse_reflectivity, specular_reflectivity, environment_reflectivity, shininess), center(center), radius(radius) {}
 
         // n unitary vector (normal vector).
         Vector3d get_normal_vector(Vector3d intersection_point) override;
@@ -55,9 +55,9 @@ namespace atividades_cg_1::objects {
         Vector3d known_point;
         Vector3d normal;
 
-        Plan(Vector3d known_point, Vector3d normal, IntensityColor difuse_reflectivity, IntensityColor specular_reflectivity, 
-        IntensityColor environment_reflectivity, float shininess, Color color)
-        : Object(color, difuse_reflectivity, specular_reflectivity, environment_reflectivity, shininess), known_point(known_point), normal(normal) {}
+        Plan(Vector3d known_point, Vector3d normal, IntensityColor difuse_reflectivity, IntensityColor specular_reflectivity,
+             IntensityColor environment_reflectivity, float shininess, Color color)
+                : Object(color, difuse_reflectivity, specular_reflectivity, environment_reflectivity, shininess), known_point(known_point), normal(normal) {}
 
         Vector3d get_normal_vector(Vector3d intersection_point) override;
 
@@ -65,27 +65,44 @@ namespace atividades_cg_1::objects {
     };
 
     class Triangle : public Object {
-        private:
-            Vector3d p1;
-            Vector3d p2;
-            Vector3d p3;
-            Vector3d r1;
-            Vector3d r2;
+    private:
+        Vector3d p1;
+        Vector3d p2;
+        Vector3d p3;
+        Vector3d r1;
+        Vector3d r2;
 
-        public:
+    public:
 
-            Triangle(Vector3d p1, Vector3d p2, 
-            Vector3d p3, Color color=Color(255,255,255), 
-            IntensityColor dr=IntensityColor(.7, .7, .7), IntensityColor sr=IntensityColor(.7, .7, .7),
-            IntensityColor er=IntensityColor(.7, .7, .7), float shininess=10) : p1(p1), p2(p2), p3(p3), Object(color,dr, sr, er, shininess) {
-                this->r1 = p2.minus(p1);
-                this->r2 = p3.minus(p1);
-            }
+        Triangle(Vector3d p1, Vector3d p2,
+                 Vector3d p3, Color color=Color(255,255,255),
+                 IntensityColor dr=IntensityColor(.7, .7, .7), IntensityColor sr=IntensityColor(.7, .7, .7),
+                 IntensityColor er=IntensityColor(.7, .7, .7), float shininess=10) : p1(p1), p2(p2), p3(p3), Object(color,dr, sr, er, shininess) {
+            this->r1 = p2.minus(p1);
+            this->r2 = p3.minus(p1);
+        }
 
-            Vector3d get_normal_vector(Vector3d intersection_point) override;
+        Vector3d get_normal_vector(Vector3d intersection_point) override;
 
 
-            Intersection get_intersection(Ray ray) override;
+        Intersection get_intersection(Ray ray) override;
+    };
+
+    class Cylinder: public Object {
+    public:
+        Vector3d base_center;
+        Vector3d top_center;
+        Vector3d cyl_direction;
+        float radius;
+        float height;
+
+        Cylinder(Vector3d base_center, Vector3d top_center, Vector3d cyl_direction, float radius, float height,IntensityColor difuse_reflectivity, IntensityColor specular_reflectivity,
+                 IntensityColor environment_reflectivity, float shininess, Color color)
+                : Object(color, difuse_reflectivity, specular_reflectivity, environment_reflectivity, shininess), base_center(base_center), top_center(top_center), cyl_direction(cyl_direction),  radius(radius), height(height) {}
+
+        Vector3d get_normal_vector(Vector3d intersection_point) override;
+
+        Intersection get_intersection(Ray ray) override;
     };
 }
 
